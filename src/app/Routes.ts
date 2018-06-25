990c9d0ce78b084d9f4c2f9c535271b473e0aff3
Michael Burke <mike.d.burke@gmail.com>
2018-06-24 21:55:38 -0400

    Initial commit

---
+++ src/app/Routes.ts
@@ -0,0 +1,18 @@
+import { Routes } from '@angular/router';
+import { QueryComponent } from './query/query.component';
+import { HomeComponent } from './home/home.component';
+
+export const AppRoutes: Routes = [
+  { path: 'query', component: QueryComponent }
+  { path: 'home',      component: HomeComponent },
+  // {
+  //   path: 'heroes',
+  //   component: HeroListComponent,
+  //   data: { title: 'Heroes List' }
+  // },
+  // { path: '',
+  //   redirectTo: '/heroes',
+  //   pathMatch: 'full'
+  // },
+  // { path: '**', component: PageNotFoundComponent }
+];
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,23 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
-
 import { AppComponent } from './app.component';
-
+import { QueryComponent } from './query/query.component';
+import { RouterModule } from '@angular/router';
+import { AppRoutes } from './Routes';
+import { HomeComponent } from './home/home.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    QueryComponent,
+    HomeComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot(
+      AppRoutes,{ enableTracing: true }
+    )
   ],
   providers: [],
   bootstrap: [AppComponent]
